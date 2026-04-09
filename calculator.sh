b0736a706e9c157c78426464f123ad5c1ac32ecb
Sourin Biswas <sourin1987@gmail.com>
2026-04-09 13:01:31 +0530

    add read function and mutiple entry for addition,mutiply

--- calculator.sh
+++ calculator.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+function read_number() {
+    read -p "Enter number1: " num1
+    read -p "Enter number2: " num2
+}
 
 while [ option != 5 ]
 do
@@ -9,27 +13,34 @@ do
         echo -e "Please choose from the given option\n"
     else
         case $option in
-            1)  read -p "Enter number1: " num1
-                read -p "Enter number2: " num2
-                answer=$(echo "scale=2;$num1 + $num2" | bc -l)
-                echo -e "Sum of two given numbers: $answer\n" ;;
+            1)  sum=0
+                while true
+                do
+                    read -p "Enter number(only Enter for result): " num
+                    if [ -z $num ]; then
+                        break
+                    fi
+                        sum=$(echo "scale=2;$sum + $num" | bc -l)
+                done
+                echo -e "Sum of two given numbers: $sum\n" ;;
 
-            2)  read -p "Enter number1: " num1
-                read -p "Enter number2: " num2
+            2)  read_number
                 answer=$(echo "scale=2;$num1 - $num2" | bc -l)
-                echo -e "Sum of two given numbers: $answer\n"
                 echo -e "Difference of two given numbers: $answer\n" ;;
 
-            3)
-                read -p "Enter number1: " num1
-                read -p "Enter number2: " num2
-                answer=$(echo "scale=2;$num1 * $num2" | bc -l)
-                echo -e "Sum of two given numbers: $answer\n"
-                echo -e "Multiplication of two given numbers: $answer\n" ;;
+            3)  multiply=1
+                while true
+                do
+                    read -p "Enter number(only Enter for result): " num
+                    if [ -z $num ]; then
+                        break
+                    fi
+                    multiply=$(echo "scale=2;$multiply * $num" | bc -l)
+                done
 
-            4)
-                read -p "Enter number1: " num1
-                read -p "Enter number2: " num2
+                echo -e "Multiplication given numbers: $multiply\n" ;;
+
+            4)  read_number
                 answer=$(echo "scale=2;$num1 / $num2" | bc -l)
                 echo -e "Division of two given numbers: $answer\n" ;;
 
